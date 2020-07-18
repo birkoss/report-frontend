e4ab38a0608b6461ca2a0e2517116d51adb7b7ff
Mathieu Robichaud <admin@birkoss.com>
2020-07-18 12:12:25 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 
-import { LandingPage } from "./pages/LandingPage";
-
-import { AppLayout } from "./components/AppLayout";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
 import { DashboardPage } from './pages/Dashboard';
@@ -11,20 +8,12 @@ import { DashboardPage } from './pages/Dashboard';
 import './App.css';
 
 
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-
-        <Switch>
-          <Route exact path="/" component={DashboardPage} />
-          <Route path="*" component={() => "404 NOT FOUND"} />
-        </Switch>
-
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={DashboardPage} />
+      <Route path="*" component={() => "404 NOT FOUND"} />
+    </Switch>
   );
 }
 
